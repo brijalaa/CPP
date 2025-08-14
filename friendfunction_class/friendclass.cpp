185c04bb2e5d025e59d2ff34cf0697c5f8c9076d
Brijala <brijalaapandey@gmail.com>
2025-08-14 20:53:21 +0545

    .

---
+++ friendfunction_class/friendclass.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class B;
+class A{
+    int x;
+    int y;
+    public:
+    void enter(){
+        cout<<"Enter x and y:"
+        cin>>x>>y;
+    }
+    friend class B;
+};
+
+class B{
+    public:
+    void display( A k){
+        cout<<k.x<<endl<<k.y;
+    }
+
+};
+
+int main(){
+    A obj1;
+    B obj2;
+    obj1.enter();
+    obj2.display(obj1);
+}
