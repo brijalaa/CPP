12bf13154e271f1f8b7100c1fc42b588d2b12b1f
Brijala <brijalaapandey@gmail.com>
2025-08-16 20:59:08 +0545

    user defined to user defined type conversion

---
+++ typeconversion/3.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class polar //source class
+{
+    float rad;
+    int angle;
+    public:
+    polar(){
+        rad=0;
+        angle=0;
+    }
+
+    polar(float r, int ang){
+        rad= r;
+        angle= ang;
+    }
+    
+    float return_rad(){
+        return rad;
+
+    }
+    
+    int return_ang(){
+        return angle;
+    }
+
+    void display(){
+        cout<<"Angle = "<<angle<<" "<<"Radius = "<<rad<<endl;
+    }
+};
+   
+class cartesian{
+        float  xco, yco;
+        public:
+        cartesian(){
+            xco=0;
+            yco=0;
+        }
+        cartesian(polar p){
+            xco= p.return_rad()* cos(p.return_ang());
+            yco = p.return_rad()* sin(p.return_ang());
+        }
+        void  display(){
+            cout<<"xco = "<<xco<<" "<<"yco = "<<yco<<endl;
+        }
+    };
+
+int main(){
+    polar p(5.5, 2);
+    cartesian c1;
+    c1 = p;
+    c1.display();
+    return 0;
+
+}
